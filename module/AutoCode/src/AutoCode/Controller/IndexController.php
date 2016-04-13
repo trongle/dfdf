37305bb763f2017f01891e02c58a8844a860845f
trongle <trongle1711@gmail.com>
2016-04-13 18:01:17 +0700

    tao code filter

--- module/AutoCode/src/AutoCode/Controller/IndexController.php
+++ module/AutoCode/src/AutoCode/Controller/IndexController.php
@@ -55,8 +55,8 @@ class IndexController extends AbstractActionController
     public function loadTemplateAction(){   
         if($this->request->isXmlHttpRequest()){
             $fileName = $this->params()->fromQuery('fileName');
-            if(file_exists(PATH_PUBLIC . 'html/' . $fileName)){
-                $templateValidate = file_get_contents(PATH_PUBLIC . 'html/' . $fileName);
+            if(file_exists(PATH_PUBLIC . 'html/validate/' . $fileName)){
+                $templateValidate = file_get_contents(PATH_PUBLIC . 'html/validate/' . $fileName);
             }else{
                 $templateValidate = 'no';
             }
@@ -70,51 +70,6 @@ class IndexController extends AbstractActionController
     }
 
     public function validateAction(){
-        // $this->add(array(
-        //     'name'    => "my-email",
-        //     "filters" =>array(
-        //         array( "name" => "StringToUpper" ),
-        //         array( "name" => "StringTrim" ),
-        //         array( "name" => "PregReplace",
-        //                "options" => array(
-        //                     "pattern" => "#[\d]#",
-        //                     "replacement" => "."
-        //                 )
-        //         )
-        //     ),
-            // "validators" => array(
-            //     array(
-            //         "name" => "NotEmpty",
-            //         "options" => array(
-            //             "messages" => array(
-            //                 \Zend\Validator\NotEmpty::IS_EMPTY => "Dữ liệu không được rỗng"
-            //             )
-            //         ),
-            //         "break_chain_on_failure" => "true"
-            //     ),
-        //         array(
-        //             "name" => "StringLength",
-        //             "options" => array(
-        //                 "min" => "3",
-        //                 "max" => "10",
-        //                 "messages" => array(
-        //                     \Zend\Validator\StringLength::TOO_SHORT => "Số ký tự phải lớn hơn hoặc bằng %min%",
-        //                     \Zend\Validator\StringLength::TOO_LONG => "Số ký tự phải nhỏ hơn hoặc bằng %max%",
-        //                 )
-        //             )
-        //         ),
-        //         array(
-        //             "name" => "EmailAddress",
-        //         )
-                
-        //     )
-            
-        // ));
-        
-        echo "<pre>";
-        print_r($this->request->getPost());
-        echo "</pre>";
-
         if($this->request->isXmlHttpRequest()){
             
             $post        = $this->request->getPost();
@@ -156,7 +111,7 @@ class IndexController extends AbstractActionController
                                     . $this->close()
                                 . $this->closeCode();
             }
-            echo '<hr/>';
+            echo '<div class="hide">superman</div>';
             echo $codeValidate;
 
             return $this->response;
@@ -165,14 +120,14 @@ class IndexController extends AbstractActionController
     }
 
     private function openValidate(){
-        return $this->setSpace() . '"validators" => array(<br/>';
+        return $this->setSpace() . '"validators" <span class="php-plain">=></span> <span class="php-keyword">array</span><span class="php-plain">(</span><br/>';
     }
 
     private function createStringValidate(){
         $codeValidate = '';
         foreach($this->validateName as $validate){
-            $codeValidate   .= $this->setSpace(2) . 'array(<br/>'
-                . $this->setSpace(3) . '"name" => "' . $this->validateName[$validate] . '",<br/>'
+            $codeValidate   .= $this->setSpace(2) . '<span class="php-keyword">array</span><span class="php-plain">(</span><br/>'
+                . $this->setSpace(3) . '"name" <span class="php-plain">=></span> "' . $this->validateName[$validate] . '"<span class="php-plain">,</span><br/>'
                 . $this->validateOptionString(strtolower($validate))
                 . $this->createBreakChainString($validate)
             . $this->close(2);
@@ -184,15 +139,18 @@ class IndexController extends AbstractActionController
     private function validateOptionString($validateName){
         $codeValidate = '';
         if(isset($this->validateOption[$validateName]) && count($this->validateOption[$validateName]) > 0){
-            $codeValidate   .= $this->setSpace(3) . '"options" => array(<br/>';
                 $codeValidateOption = '';
                 foreach($this->validateOption[$validateName] as $name => $value){
                     if($value == 'on') $value = "true";
-                    $codeValidateOption   .= $this->setSpace(4) . '"' . $name . '" => "' . $value . '",<br/>';
+                    if(empty($value)) continue;
+                    $codeValidateOption   .= $this->setSpace(4) . '"' . $name . '" <span class="php-plain">=></span> "' . $value . '"<span class="php-plain">,</span><br/>';
                     
                 }
-                $codeValidate .= $codeValidateOption;
-            $codeValidate   .= $this->close(3);       
+                if(!empty(trim($codeValidateOption))){
+                    $codeValidate   .= $this->setSpace(3) . '"options" <span class="php-plain">=></span> <span class="php-keyword">array</span><span class="php-plain">(</span><br/>';
+                        $codeValidate .= $codeValidateOption;
+                    $codeValidate   .= $this->close(3);       
+                }
         }
 
         return $codeValidate;
@@ -204,26 +162,26 @@ class IndexController extends AbstractActionController
             $breakChain = 'true';
         }
 
-        return $this->setSpace(3) . '"break_chain_on_failure" => "' . $breakChain . '"<br/>';
+        return $this->setSpace(3) . '"break_chain_on_failure" <span class="php-plain">=></span> "' . $breakChain . '"<br/>';
     }
 
     private function openAttribute(){
-        return self::setSpace().'"attributes" => array(<br/>';
+        return self::setSpace().'"attributes" <span class="php-plain">=></span> <span class="php-keyword">array</span><span class="php-plain">(</span><br/>';
     }
 
     private function openOption(){
-        return self::setSpace().'"options" => array(<br/>' ;
+        return self::setSpace().'"options" <span class="php-plain">=></span> <span class="php-keyword">array</span><span class="php-plain">(</span><br/>' ;
     }
 
     private function close($level = 1){
-        return self::setSpace($level).'),<br/>';
+        return self::setSpace($level).'<span class="php-plain">),</span><br/>';
     }
     
     private function labelAttribute(){
         if(!empty($this->classLabel) || !empty($this->forLabel)){
-            return self::setSpace(2).'"label_attributes" => array(<br/>' 
+            return self::setSpace(2).'"label_attributes" <span class="php-plain">=></span> <span class="php-keyword">array</span><span class="php-plain">(</span><br/>' 
                                     . $this->classLabel . $this->forLabel
-                                    . self::setSpace(2).'),<br/>';
+                                    . self::setSpace(2).'<span class="php-plain">),</span><br/>';
         }else{
             return null;
         }
@@ -232,21 +190,21 @@ class IndexController extends AbstractActionController
     private function setValue($post = null){
         if(!empty($post)){
 
-            $this->type     = empty($post["typeElement"])? '' :self::setSpace().'"type" => "'.$post["typeElement"].'",<br/>';
-            $this->name     = empty($post["nameElement"])? '' :self::setSpace().'"name" => "'.$post['nameElement'].'",<br/>';
-            $this->required = empty($post['requiredElement'])? self::setSpace().'"required" => false,<br/>' :self::setSpace().'"required" => true,<br/>';
+            $this->type     = empty($post["typeElement"])? '' :self::setSpace().'"type" <span class="php-plain">=></span> "'.$post["typeElement"].'"<span class="php-plain">,</span><br/>';
+            $this->name     = empty($post["nameElement"])? '' :self::setSpace().'"name" <span class="php-plain">=></span> "'.$post['nameElement'].'"<span class="php-plain">,</span><br/>';
+            $this->required = empty($post['requiredElement'])? self::setSpace().'"required" <span class="php-plain">=></span> false,<br/>' :self::setSpace().'"required" <span class="php-plain">=></span> true,<br/>';
             
             //attribute
-            $this->class           = empty($this->attribute['class'])? '' : self::setSpace(2).'"class" => "' . $this->attribute['class'] . '",<br/>';
-            $this->id              = empty($this->attribute['id'])? '' : self::setSpace(2).'"id" => "' . $this->attribute['id'] . '",<br/>';
-            $this->placeholder     = empty($this->attribute['placeholder'])? '' : self::setSpace(2).'"placeholder" => "' . $this->attribute['placeholder'] . '",<br/>';
-            $this->value           = empty($this->attribute['value'])? '' : self::setSpace(2).'"value" => "' . $this->attribute['value'] . '",<br/>';
+            $this->class           = empty($this->attribute['class'])? '' : self::setSpace(2).'"class" <span class="php-plain">=></span> "' . $this->attribute['class'] . '"<span class="php-plain">,</span><br/>';
+            $this->id              = empty($this->attribute['id'])? '' : self::setSpace(2).'"id" <span class="php-plain">=></span> "' . $this->attribute['id'] . '"<span class="php-plain">,</span><br/>';
+            $this->placeholder     = empty($this->attribute['placeholder'])? '' : self::setSpace(2).'"placeholder" <span class="php-plain">=></span> "' . $this->attribute['placeholder'] . '"<span class="php-plain">,</span><br/>';
+            $this->value           = empty($this->attribute['value'])? '' : self::setSpace(2).'"value" <span class="php-plain">=></span> "' . $this->attribute['value'] . '"<span class="php-plain">,</span><br/>';
             $this->otherAttributes = $this->setOtherAttributes();
             
             //options
-            $this->nameLabel  = empty($this->option['nameLabel'])? '' : self::setSpace(2).'"label" => "' . $this->option['nameLabel'] . '",<br/>';
-            $this->classLabel = empty($this->option['classLabel'])? '' : self::setSpace(3).'"class" => "' . $this->option['classLabel'] . '",<br/>';
-            $this->forLabel   = empty($this->option['forLabel'])? '' : self::setSpace(3).'"for" => "' . $this->option['forLabel'] . '",<br/>';
+            $this->nameLabel  = empty($this->option['nameLabel'])? '' : self::setSpace(2).'"label" <span class="php-plain">=></span> "' . $this->option['nameLabel'] . '"<span class="php-plain">,</span><br/>';
+            $this->classLabel = empty($this->option['classLabel'])? '' : self::setSpace(3).'"class" <span class="php-plain">=></span> "' . $this->option['classLabel'] . '"<span class="php-plain">,</span><br/>';
+            $this->forLabel   = empty($this->option['forLabel'])? '' : self::setSpace(3).'"for" <span class="php-plain">=></span> "' . $this->option['forLabel'] . '"<span class="php-plain">,</span><br/>';
         }
     }
 
@@ -256,7 +214,7 @@ class IndexController extends AbstractActionController
     }
 
     private function setValueSelectBox(){
-        $this->emptyOption = empty($this->option['emptyOption'])? '' : self::setSpace(2).'"empty_option" => "' . $this->option['emptyOption'] . '",<br/>';                
+        $this->emptyOption = empty($this->option['emptyOption'])? '' : self::setSpace(2).'"empty_option" <span class="php-plain">=></span> "' . $this->option['emptyOption'] . '"<span class="php-plain">,</span><br/>';                
         $this->valueOption = empty($this->option['valueOption'])? '' :  $this->option['valueOption'];                
         $this->nameOption  = empty($this->option['nameOption'])? '' :  $this->option['nameOption'];                
         
@@ -274,13 +232,13 @@ class IndexController extends AbstractActionController
                 $this->valueOption = explode(',',$this->valueOption);
                 $this->nameOption  = explode(',',$this->nameOption);
 
-                $selectOption .= self::setSpace(2).'"value_option" => array(<br/>';
+                $selectOption .= self::setSpace(2).'"value_option" <span class="php-plain">=></span> <span class="php-keyword">array</span><span class="php-plain">(</span><br/>';
 
                 foreach($this->valueOption as $key => $val){
-                    $selectOption .= self::setSpace(3).'"'. $val .'" => "' . $this->nameOption[$key] . '",<br/>';
+                    $selectOption .= self::setSpace(3).'"'. $val .'" <span class="php-plain">=></span> "' . $this->nameOption[$key] . '"<span class="php-plain">,</span><br/>';
                 }
 
-                $selectOption .= self::setSpace(2).'),<br/>' ;
+                $selectOption .= self::setSpace(2).'<span class="php-plain">),</span><br/>' ;
             }
 
 
@@ -291,11 +249,11 @@ class IndexController extends AbstractActionController
     }
 
     private  function openCode($tag = '<code>'){
-        return $tag . '$this->add(array(<br/>' ;
+        return $tag . '<span class="php-variable">$this</span><span class="php-plain">->add</span><span class="php-plain">(</span><span class="php-keyword">array</span><span class="php-plain">(</span><br/>' ;
     }
 
     private  function closeCode($closeTag = '</code>'){
-        return '));' . $closeTag . '<br/>';
+        return '<span class="php-plain">));</span>' . $closeTag . '<br/>';
     }
 
     private function setOtherAttributes(){
@@ -307,7 +265,7 @@ class IndexController extends AbstractActionController
             foreach($attrs as $attr){
                 $attr  = trim($attr);
                 $attr  = explode(':',$attr);
-                $xhtml .=  self::setSpace(2).'"' . $attr[0] . '" => "' . $attr[1] . '",<br/>';
+                $xhtml .=  self::setSpace(2).'"' . @$attr[0] . '" <span class="php-plain">=></span> "' . @$attr[1] . '"<span class="php-plain">,</span><br/>';
             }
         }
 
